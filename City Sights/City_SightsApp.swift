5d030e877f695a13bb5324596f0204f9fbb8aa13
Lyndi Castrejon <lyndic@protonmail.com>
2022-05-03 16:00:41 -0500

    Initial Commit

---
+++ City Sights/City_SightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_SightsApp.swift
+//  City Sights
+//
+//  Created by Lyndi Castrejon on 5/3/22.
+//
+
+import SwiftUI
+
+@main
+struct City_SightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ City SightsUITests/City_SightsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  City_SightsUITestsLaunchTests.swift
+//  City SightsUITests
+//
+//  Created by Lyndi Castrejon on 5/3/22.
+//
+
+import XCTest
+
+class City_SightsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
