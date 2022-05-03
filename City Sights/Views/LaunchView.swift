726e9975d2f2015f9b479a9b3fa5e2d8b048b896
Lyndi Castrejon <lyndic@protonmail.com>
2022-05-03 16:05:06 -0500

    - Rename ContentView to LaunchView - Initial app setup

--- City Sights/Views/LaunchView.swift
+++ City Sights/Views/LaunchView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  LaunchView.swift
 //  City Sights
 //
 //  Created by Lyndi Castrejon on 5/3/22.
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LaunchView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct LaunchView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LaunchView()
     }
 }
