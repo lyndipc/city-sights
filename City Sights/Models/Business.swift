11cf3259d50e13a6e1dfa51d1071e113b02bc345
Lyndi Castrejon <lyndic@protonmail.com>
2022-05-04 15:32:34 -0500

    - Created Business model

---
+++ City Sights/Models/Business.swift
@@ -0,0 +1,16 @@
+//
+//  Business.swift
+//  City Sights
+//
+//  Created by Lyndi Castrejon on 5/4/22.
+//
+
+import Foundation
+
+struct Business: Decodable {
+    
+    var id: String?
+    var alias: String?
+    var name: String?
+    var image_url: String?
+}
