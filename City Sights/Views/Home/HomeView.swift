ffda9c39a6e9c31c22fdc984cecafb29c7900579
Lyndi Castrejon <lyndic@protonmail.com>
2022-05-05 00:06:09 -0500

    Created HomeView, added state property for location services authorization, and created conditional transition on LaunchView

---
+++ City Sights/Views/Home/HomeView.swift
@@ -0,0 +1,47 @@
+//
+//  HomeView.swift
+//  City Sights
+//
+//  Created by Lyndi Castrejon on 5/4/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @EnvironmentObject var model: ContentModel
+    
+    @State var isMapShowing = false
+    
+    var body: some View {
+        
+        if model.restaurants.count != 0 || model.sights.count != 0 {
+            
+            // Determine if list or map should be displayed
+            if !isMapShowing {
+                
+                // Show list
+                VStack {
+                    HStack {
+                        Image(systemName: "mappin")
+                        Text("San Francisco")
+                        Spacer()
+                        Text("Switch to map view")
+                    }
+                }
+            }
+            else {
+                // Show map
+            }
+        }
+        else {
+            ProgressView()
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
--- City Sights/Views/LaunchView.swift
+++ City Sights/Views/LaunchView.swift
@@ -8,7 +8,29 @@
 import SwiftUI
 
 struct LaunchView: View {
+    
+    @EnvironmentObject var model: ContentModel
+    
     var body: some View {
+        
+        // Detect the authorization status of geolocating the user
+        
+        
+        if model.authorizationState == .notDetermined {
+        
+            // If undetermined, show onboarding
+        }
+        else if model.authorizationState == .authorizedWhenInUse || model.authorizationState == .authorizedAlways {
+        
+            // If approved, show home view
+            HomeView()
+        }
+        else if model.authorizationState == .denied {
+            
+            // If denied, show Denied view
+        }
+        
+        // If denied, show denied view
         Text("Hello, world!")
             .padding()
     }
