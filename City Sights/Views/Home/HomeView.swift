c414fecd0e8a5af0d8f8d5d5ea085a9599734421
Lyndi Castrejon <lyndic@protonmail.com>
2022-05-08 17:49:16 -0500

    Add List View toggle button on MapView

--- City Sights/Views/Home/HomeView.swift
+++ City Sights/Views/Home/HomeView.swift
@@ -39,15 +39,40 @@ struct HomeView: View {
                     .navigationBarHidden(true)
                 }
                 else {
-                    // Show map
-                    BusinessMap(selectedBusiness: $selectedBusiness)
-                        .ignoresSafeArea()
-                        .sheet(item: $selectedBusiness) { business in
+                    
+                    ZStack(alignment: .top) {
+                     
+                        // Show map
+                        BusinessMap(selectedBusiness: $selectedBusiness)
+                            .ignoresSafeArea()
+                            .sheet(item: $selectedBusiness) { business in
+                                
+                                // Create a BusinessDetail instance
+                                // Pass in selected business
+                                BusinessDetail(business: business)
+                            }
+                        
+                        // Rectangle overlay
+                        ZStack {
                             
-                            // Create a BusinessDetail instance
-                            // Pass in selected business
-                            BusinessDetail(business: business)
+                            Rectangle()
+                                .foregroundColor(.white)
+                                .cornerRadius(5)
+                                .frame(height: 48)
+                            
+                            HStack {
+                                Image(systemName: "mappin")
+                                Text("San Francisco")
+                                Spacer()
+                                Button("List View") {
+                                    self.isMapShowing = false
+                                }
+                            }
+                            .padding()
                         }
+                        .padding()
+                    }
+                    .navigationBarHidden(true)
                 }
             }
         }
